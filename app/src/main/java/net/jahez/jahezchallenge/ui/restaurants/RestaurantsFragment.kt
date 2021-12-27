6c06155560fe285766229891d47a2c9a601b0310
Mohamed <ashourm47@gmail.com>
2021-12-27 20:47:31 +0300

    apply filter

--- app/src/main/java/net/jahez/jahezchallenge/ui/restaurants/RestaurantsFragment.kt
+++ app/src/main/java/net/jahez/jahezchallenge/ui/restaurants/RestaurantsFragment.kt
@@ -1,15 +1,15 @@
 package net.jahez.jahezchallenge.ui.restaurants
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import net.jahez.jahezchallenge.MyApp
 import net.jahez.jahezchallenge.base.BaseFragment
 import net.jahez.jahezchallenge.databinding.FragmentRestaurantsBinding
+import android.view.MenuInflater
+import net.jahez.jahezchallenge.R
 
 @AndroidEntryPoint
 class RestaurantsFragment : BaseFragment() {
@@ -36,6 +36,7 @@ class RestaurantsFragment : BaseFragment() {
     }
 
     private fun init() {
+        setHasOptionsMenu(true)
         val gridLayoutManager = GridLayoutManager(MyApp.appContext, 2, GridLayoutManager.VERTICAL, false)
         mBinding.rvIngredients.layoutManager = gridLayoutManager
         ingredientsAdapter = RestaurantsAdapter()
@@ -44,6 +45,29 @@ class RestaurantsFragment : BaseFragment() {
 
     private fun initObserving() {
         viewModel.restaurantsList.observe(viewLifecycleOwner, { ingredientsAdapter.submitList(it) })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.filter_options_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_offers -> {
+                ingredientsAdapter.submitList(viewModel.restaurantsList.value!!.filter { it.hasOffer })
+                true
+            }
+            R.id.action_distance -> {
+                ingredientsAdapter.submitList(viewModel.restaurantsList.value!!.sortedByDescending { it.distance })
+                true
+            }
+            R.id.action_all -> {
+                ingredientsAdapter.submitList(viewModel.restaurantsList.value!!)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
